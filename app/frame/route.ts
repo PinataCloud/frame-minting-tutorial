5bac9483421a27fc9bd6b4d44192b07441803642
Steve <stevedsimkins@gmail.com>
2024-02-14 23:02:58 -0500

    updated button

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 import { getConnectedAddressForUser } from "@/utils/fc";
 import { mintNft, balanceOf } from "@/utils/mint";
 
@@ -34,8 +34,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
       const mint = await mintNft(address);
       console.log(mint);
       const frameMetadata = await fdk.getFrameMetadata({
-        post_url: `${process.env.BASE_URL}/frame`,
-        buttons: [{ label: "Go Back", action: "post" }],
+        post_url: `${process.env.BASE_URL}/redirect`,
+        buttons: [{ label: "Learn How to Make This", action: "post_redirect" }],
         aspect_ratio: "1:1",
         cid: "QmUx3kQH4vR2t7mTmW3jHJgJgJGxjoBsMxt6z1fkZEHyHJ",
       });
