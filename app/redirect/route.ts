8d51debe7c929f520fad60bb0be427ab91bc04c6
Steve <stevedsimkins@gmail.com>
2024-02-16 13:14:53 -0500

    Added blog post link

--- app/redirect/route.ts
+++ app/redirect/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse){
   try {
-   return NextResponse.redirect('https://pinata.cloud', { status: 302 }) 
+   return NextResponse.redirect('https://www.pinata.cloud/blog/how-to-build-a-farcaster-frame-that-mints-nfts', { status: 302 }) 
   } catch (error) {
    console.log(error) 
     return NextResponse.json({ error: error })
