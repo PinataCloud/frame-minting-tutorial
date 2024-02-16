edae1cc0678db7b048496bdbc41f9a40985749dd
Steve <stevedsimkins@gmail.com>
2024-02-16 13:59:08 -0500

    small bug fix

--- app/redirect/route.ts
+++ app/redirect/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
   const buttonId = body.untrustedData.buttonIndex;
   if (buttonId === 1) {
     try {
